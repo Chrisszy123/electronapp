92e92fda0767b6349d05ed3f54a1744b64c92de1
Chrisszy123 <bassygoodluck@gmail.com>
2023-08-25 15:19:48 +0100

    feat/ first desktop app commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require("electron")
+const url = require("url")
+const path = require("path")
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Desktop App",
+        width: 1000,
+        height: 600
+    })
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file"
+    })
+
+    mainWindow.loadURL(startUrl)
+}
+
+app.whenReady().then(createMainWindow)
